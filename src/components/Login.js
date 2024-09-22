440b403564a69940f4fa749ba9c1b8a26c7783f9
U~Iriamuzu <daviswill264@gmail.com>
2024-09-22 05:56:13 +0100

    W Third Commit

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+// import styled from 'styled-components'
+
+import { Container } from 'postcss'
+import React from 'react'
+
+const Login = (props) => {
+  return (
+    <Container>
+      <Nav>
+        <a href="/">
+          <img src="/images/login-logo.svg" alt="" />
+        </a>
+      </Nav>
+    </Container>
+  )
+}
+
+// const Container = styled.div``;
+
+// const Nav = styled.nav``;
+
+
+export default Login
