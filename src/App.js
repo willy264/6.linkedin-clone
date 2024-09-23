1f73ef232dca222086785d4277ac4b9d51932338
U~Iriamuzu <daviswill264@gmail.com>
2024-09-23 08:12:28 +0100

    W Fourth Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Header  from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -8,10 +10,17 @@ function App() {
       <Router>
         <Routes>
           <Route exact path='/' element={<Login />}/>
+          <Route path='/home' element={
+            <div>
+              <Header />
+              <Home />
+            </div>
+          } />
+
         </Routes>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,5 @@
-// import styled from 'styled-components'
-
-import { Container } from 'postcss'
-import React from 'react'
+import styled from 'styled-components'
+ import React from 'react'
 
 const Login = (props) => {
   return (
@@ -10,14 +8,199 @@ const Login = (props) => {
         <a href="/">
           <img src="/images/login-logo.svg" alt="" />
         </a>
+        <div>
+            <Join><button>Join Now</button></Join>
+            <SignIn><button>Sign In</button></SignIn>
+        </div>
       </Nav>
+      <Section>
+        <Hero>
+          <h1>Welcome to your professional community</h1>
+          <img src="/images/login-hero.svg" alt="" />
+        </Hero>
+        <Form>
+          <Google>
+            <img src="/images/google.svg" alt="" />
+            sign in with google
+          </Google>
+        </Form>
+      </Section>
     </Container>
-  )
-}
+  );
+};
 
-// const Container = styled.div``;
+const Container = styled.div`
+  padding: 0px;
+`;
 
-// const Nav = styled.nav``;
+const Nav = styled.nav`
+  max-width: 1128px;
+  margin: auto;
+  padding: 12px 0 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+
+  & > a {
+    width: 135px;
+    height: 34px;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+  }
+`;
+
+const Join = styled.a`
+  padding: 10px 12px;
+  text-decoration: none;
+  border-radius: 4px;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  cursor: pointer;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.9);
+    text-decoration: none;
+  };
+
+  button {
+    border-radius: 4px;
+    background-color: rgba(0, 0, 0, 0);
+    border: none;    
+    transition-duration: 167ms;
+    font-size: 16px;
+    font-weight: 600;
+    text-align: center;    
+    padding: 10px 12px;
+    
+    &:active {
+      scale: .9;
+    }
+  }
+`;
+
+const SignIn = styled.a`
+  line-height: 40px;
+  padding: 10px 0px;
+  border-radius: 24px;
+  background-color: rgba(0, 0, 0, 0);
+  cursor: pointer;
+  &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    color: #0a66c2;
+    text-decoration: none;
+  };
+
+  button {
+    color: #0a66c2;
+    border-radius: 24px;
+    box-shadow: inset 0 0 0 1px #0a66c2;
+    background-color: rgba(0, 0, 0, 0);
+    border: none;    
+    transition-duration: 167ms;
+    font-size: 16px;
+    font-weight: 600;
+    padding: 10px 24px;
+    text-align: center;    
+    
+    &:active {
+      scale: .9;
+    }
+  }
+
+`;
+
+const Section = styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+
+const Hero = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    text-transform: capitalize;
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+
+  img {
+    /* z-index: -1; */
+    width: 700px;
+    height: 670px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+    @media (max-width: 768px) {
+      top: 230px;
+      width: initial;
+      position: initial;
+      height: initial;
+    }
+  }
+`;
+
+const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: #fff;
+  align-items: center;
+  text-transform: capitalize;
+  gap: 10px;
+  height: 56px;
+  width: 100%;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
+    inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0);
+
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  cursor: pointer;
+  color: rgba(0, 0, 0, 0.6);
+  &:hover:active {
+    scale: .9;
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
+  }
+`;
+
+
 
 
 export default Login
